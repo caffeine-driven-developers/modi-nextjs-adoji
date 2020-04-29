4ea0e1a8dca8b7d680dfc167f209f60c35e39ed3
Dong-jin Ahn <adoji92@gmail.com>
2020-04-30 01:47:52 +0900

    Add search text input @ Navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,22 @@
-import { Button, Navbar, Alignment } from '@blueprintjs/core';
+import { Button, Navbar, Alignment, InputGroup } from '@blueprintjs/core';
 import Link from 'next/link';
+import { useCallback, FormEvent, useState } from 'react';
+import { useRouter } from 'next/dist/client/router';
 
 export default function MyNavbar() {
+  const [searchString, setSearchString] = useState('');
+  const router = useRouter();
+  const handleSubmitSearch = useCallback(
+    (evt: FormEvent) => {
+      evt.preventDefault();
+
+      // TODO: change title to search
+      router.push('/movies?title=' + searchString);
+      setSearchString('');
+    },
+    [searchString],
+  );
+
   return (
     <div>
       <Navbar>
@@ -14,11 +29,29 @@ export default function MyNavbar() {
 
           <Navbar.Divider />
 
+          <Link href="/movies">
+            <Button className="bp3-minimal" icon="build">
+              Movies
+            </Button>
+          </Link>
+
           <Link href="/counter">
             <Button className="bp3-minimal" icon="build">
               Counter
             </Button>
           </Link>
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <form onSubmit={handleSubmitSearch}>
+            <InputGroup
+              leftIcon="search"
+              type="search"
+              placeholder="e.g. Frozen"
+              onChange={e => {
+                setSearchString(e.target.value);
+              }}
+            />
+          </form>
         </Navbar.Group>
       </Navbar>
     </div>
