921cefae7bf5e3b3d5052fd652357a01aed4be52
Dong-jin Ahn <adoji92@gmail.com>
2020-04-30 10:56:27 +0900

    Replace skeleton with SearchedMovieCard

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,15 +1,17 @@
 import { useRouter } from 'next/dist/client/router';
 import { useMovies } from '../../src/services/movies';
+import SearchedMovieCard from '../../src/components/SearchedMovieCard';
 
 type Query = string | null;
 export default function Movies() {
   const router = useRouter();
   const title = router.query.title as Query;
+  const { data } = useMovies([title]);
+
   if (!title) {
     return <div>plaese search</div>;
   }
 
-  const { data } = useMovies([title]);
   if (!data) {
     return <div>loading...</div>;
   }
@@ -20,9 +22,18 @@ export default function Movies() {
 
   return (
     <div className="container">
+      <h1>Search result for "{title}"</h1>
       {data.Search.map(x => (
-        <p key={x.imdbID}>{x.Title}</p>
+        <SearchedMovieCard className="s-movie-card" key={x.imdbID} {...x} />
       ))}
+      <style jsx global>{`
+        .s-movie-card {
+          margin-bottom: 1rem;
+          margin-right: 1rem;
+          width: 15rem;
+          display: inline-block;
+        }
+      `}</style>
     </div>
   );
 }
