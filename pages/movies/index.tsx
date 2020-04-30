7f5e9020fe1d64b0c287075d8205c0a9fedd10f2
Dong-jin Ahn <adoji92@gmail.com>
2020-04-30 19:28:05 +0900

    Improve movies page (search visual cue, loading)

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,19 +1,58 @@
 import { useRouter } from 'next/dist/client/router';
-import { useMovies } from '../../src/services/movies';
+import { useSearch } from '../../src/services/search';
 import SearchedMovieCard from '../../src/components/SearchedMovieCard';
+import { Callout, Icon, Spinner } from '@blueprintjs/core';
 
 type Query = string | null;
 export default function Movies() {
   const router = useRouter();
-  const title = router.query.title as Query;
-  const { data } = useMovies([title]);
+  const s = router.query.search as Query;
+  const { data } = useSearch(s);
 
-  if (!title) {
-    return <div>plaese search</div>;
+  if (!s) {
+    if ((global as any).document) {
+      const navbarSearchInputElement = document.getElementById('navbar_search_input');
+      if (navbarSearchInputElement) {
+        setTimeout(() => {
+          navbarSearchInputElement.focus(); // NOTE: to avoid unstable update
+        }, 50);
+      }
+    }
+
+    return (
+      <div className="wrapper">
+        <h3 className="bp3-heading">
+          Search movie title looking for <Icon style={{ paddingBottom: 4 }} icon="arrow-up" />
+        </h3>
+
+        <style jsx>{`
+          .wrapper {
+            padding-top: 0.5rem;
+            padding-right: 4rem;
+            padding-left: 4rem;
+            float: right;
+            width: 27rem;
+            background-color: lightgray;
+            border-bottom-left-radius: 5px;
+            text-align: center;
+          }
+        `}</style>
+      </div>
+    );
   }
 
   if (!data) {
-    return <div>loading...</div>;
+    return (
+      <div className="wrapper">
+        <Spinner intent="primary" />
+        <style jsx>{`
+          .wrapper {
+            padding-top: 30vh;
+            text-align: center;
+          }
+        `}</style>
+      </div>
+    );
   }
 
   if (data.Response === 'False') {
@@ -22,7 +61,7 @@ export default function Movies() {
 
   return (
     <div className="container">
-      <h1>Search result for "{title}"</h1>
+      <h1>Search result for "{s}"</h1>
       {data.Search.map(x => (
         <SearchedMovieCard className="s-movie-card" key={x.imdbID} {...x} />
       ))}
