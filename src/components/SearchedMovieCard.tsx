348385273c75b10ef93381633ade6a511bf4e267
Dong-jin Ahn <adoji92@gmail.com>
2020-04-30 21:49:15 +0900

    Modularize SearchedMovieCard style

--- src/components/SearchedMovieCard.tsx
+++ src/components/SearchedMovieCard.tsx
@@ -11,18 +11,28 @@ type SearchedMovieCardProps = {
 
 export default function SearchedMovieCard(props: SearchedMovieCardProps) {
   return (
-    <Card className={props.className} interactive={true} elevation={Elevation.TWO}>
-      <h5 className="title bp3-heading">
-        {props.Title} ({props.Year})
-      </h5>
-      <img className="img-fluid" src={props.Poster} />
-      <style jsx scoped>{`
+    <div className="wrapper">
+      <Card className={props.className} interactive={true} elevation={Elevation.TWO}>
+        <h5 className="title bp3-heading">
+          {props.Title} ({props.Year})
+        </h5>
+        <img className="img-fluid" src={props.Poster} />
+      </Card>
+
+      <style jsx>{`
+        .wrapper {
+          margin-bottom: 1rem;
+          margin-right: 1rem;
+          width: 15rem;
+          display: inline-block;
+        }
+
         .title {
           text-overflow: ellipsis;
           overflow: hidden;
           white-space: nowrap;
         }
       `}</style>
-    </Card>
+    </div>
   );
 }
