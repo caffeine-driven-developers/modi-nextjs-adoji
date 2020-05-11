e5f9aabe0f2022dacca118c4627af8f30ea50b5a
Dong-jin Ahn <adoji92@gmail.com>
2020-05-11 16:43:37 +0900

    Replace blueprint Alert, Card with antd's

--- src/components/SearchedMovieCard.tsx
+++ src/components/SearchedMovieCard.tsx
@@ -1,7 +1,6 @@
-import { Card, Elevation } from '@blueprintjs/core';
 import { useState, useCallback } from 'react';
 import { SearchedMovie, useMovie } from '../services/omdb';
-import { Alert } from '@blueprintjs/core';
+import { Modal, Card } from 'antd';
 
 type SearchedMovieCardProps = SearchedMovie & {};
 
@@ -14,32 +13,28 @@ export default function SearchedMovieCard(props: SearchedMovieCardProps) {
 
   return (
     <div className="wrapper">
-      <Card interactive elevation={Elevation.TWO} onClick={handleClickCard}>
-        <h5 className="title bp3-heading">
-          {props.Title} ({props.Year})
-        </h5>
-        <img className="img-fluid" src={props.Poster} />
+      <Card onClick={handleClickCard} hoverable cover={<img className="img-fluid" src={props.Poster} />}>
+        <Card.Meta title={`${props.Title} (${props.Year})`}></Card.Meta>
       </Card>
 
-      <Alert
-        className="alert"
-        isOpen={isModalOpen}
-        canEscapeKeyCancel
-        canOutsideClickCancel
-        onClose={() => {
-          setIsModalOpen(false);
-        }}
-        confirmButtonText="Close"
-        style={{ width: '60vw', maxWidth: '100vw' }}
-      >
-        {data && (
-          <div className="modal-content">
-            <h1 className="bp3-heading title">
-              {data.Production} - {data.Title} ({data.Year})asdfasdfasdfasdf
-            </h1>
-          </div>
-        )}
-      </Alert>
+      {data && (
+        <Modal
+          title={`${data.Production} - ${data.Title} (${data.Year})`}
+          visible={isModalOpen}
+          maskClosable={true}
+          okText="Add to list"
+          onOk={() => {
+            // TODO:
+            console.log('added!');
+            setIsModalOpen(false);
+          }}
+          onCancel={() => {
+            setIsModalOpen(false);
+          }}
+        >
+          hello modal
+        </Modal>
+      )}
 
       <style jsx>{`
         .wrapper {
